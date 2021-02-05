3d4888b2c1de949d5e1957568031f3436006f769
mks-94 <muralikrishna9424.com>
2021-02-05 22:42:11 +0530

    Implemented display created card and able to edit created cards.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,10 @@
-import postMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
+
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
   try {
-    const postMessages = await postMessage.find();
+    const postMessages = await PostMessage.find();
 
     res.status(200).json(postMessages);
   } catch (error) {
@@ -13,7 +15,7 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new postMessage(post);
+  const newPost = new PostMessage(post);
   try {
     await newPost.save();
 
@@ -21,4 +23,18 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(401).send("No post with that id.");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };
