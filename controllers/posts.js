d316e9b1461199bca3c9d61c15531006f8561b52
mks-94 <muralikrishna9424.com>
2021-02-13 21:23:45 +0530

    Implemented auth frontend and post req got fail now need to verify.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,3 @@
-import { json } from "body-parser";
 import mongoose from "mongoose";
 
 import PostMessage from "../models/postMessage.js";
@@ -16,7 +15,11 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new PostMessage(post);
+  const newPost = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
   try {
     await newPost.save();
 
